400b839318c39fa89060307aafd282bfd4e98a44
Nana Jusolihun <nanajusolihun@gmail.com>
2023-11-22 19:06:13 +0700

    Adding meta in all page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customer",
+};
+
 export default function Page() {
   return (
     <div>
       <h1 className="bg-rose-500 p-4 text-2xl font-semibold">
         Dashboard <span className="italic font-bold font-sans">CUSTOMER</span> Page
-      </h1>;
+      </h1>     
     </div>
   );
 }
